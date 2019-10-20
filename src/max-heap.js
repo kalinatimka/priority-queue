2425307bd48cb6379d21c4b577a867aee0ce0934
Timofey <kalinata0799@gmail.com>
2019-10-20 22:55:32 +0300

    feat: add task

--- src/max-heap.js
+++ src/max-heap.js
@@ -4,12 +4,13 @@ class MaxHeap {
 	constructor() {
 		this.root = null;
 		this.parentNodes = [];
-		
+		this.length = 0;
 	}
 
 	push(data, priority) {
 		this.insertNode(new Node(data, priority));
 		this.shiftNodeUp(new Node(data, priority));
+		this.length++;
 	}
 
 	pop() {
@@ -37,26 +38,39 @@ class MaxHeap {
 	}
 
 	insertNode(node) {
-		if (this.root == null) {
+		if(this.root==null){
 			this.root = node;
 			this.parentNodes.push(node);
+			return;
 		}
-		else {
-			var index = 0;
-			while (true) {
-				if (this.parentNodes[index].left && this.parentNodes[index].right) {
-					index++;
-				}
-				else {
-                    this.parentNodes[index].appendChild(node);
-                    this.parentNodes.push(node);
-                    break;
-				}
-			}
+		else if(this.parentNodes[0].left==null){
+			this.parentNodes[0].appendChild(node);
+			this.parentNodes.push(node);
+			return;
+		}
+		else if(this.parentNodes[0].right==null){
+			this.parentNodes[0].appendChild(node);
+			this.parentNodes.push(node);
+			this.parentNodes.shift();
 		}
 	}
 
 	shiftNodeUp(node) {
+		if(node.parent!=null&&node.priority>node.parent.priority){
+			if (node.parent == this.root) {
+				this.root = node;
+			}
+			var nodeInd = this.parentNodes.indexOf(node);
+			var parentInd = this.parentNodes.indexOf(node.parent);
+			if (parentInd == -1) {
+			  this.parentNodes[nodeInd] = node.parent;
+			} else {
+			  this.parentNodes[parentInd] = node;
+			  this.parentNodes[nodeInd] = node.parent;
+			}
+			node.swapWithParent();
+			this.shiftNodeUp(node);
+		}
 	}
 
 	shiftNodeDown(node) {
